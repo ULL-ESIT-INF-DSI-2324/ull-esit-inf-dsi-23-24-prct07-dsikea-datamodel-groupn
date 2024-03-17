e3974eddb0bd0df809a2ba2bfda51d575427253e
Alejandro Javier <alu0101487168@ull.edu.es>
2024-03-17 23:58:04 +0000

    tests

--- tests/furniture.spec.ts
+++ tests/furniture.spec.ts
@@ -7,17 +7,9 @@ import { Tables } from '../src/furnitures/tables.js';
 
 describe("getSalesHistoryForClient", () => {
   const furniture = new Tables(1, 'mesa', 'roka', 'redonda', {width: 1, height: 1, length: 1}, 100);
-        it('should add new furniture', () => {
-             const furnitures = new Furniture(new Map<number, furnitureInterface>);
-             expect(furnitures.furnitureAdd(furniture.id, furniture)).to.be.an.instanceOf(undefined);
-         });
          it('should return the furniture by his id', () => {
              const furnitures = new Furniture(new Map<number, furnitureInterface>);
              expect(furnitures.getKey(furniture.id)).to.equal(undefined);
-         });
-        it('should add new furniture', () => {
-             const furnitures = new Furniture(new Map<number, furnitureInterface>);
-             expect(furnitures.furnitureAdd(furniture.id, furniture)).to.be.an.instanceOf(undefined);
          });
          it('should return the furniture by his id', () => {
              const furnitures = new Furniture(new Map<number, furnitureInterface>);
